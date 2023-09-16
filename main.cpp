b55d7462035300894a68904b97a2f889661263d7
ashok khandhar <ashokkhandhar18@gmail.com>
2023-09-17 01:07:31 +0530

    added database for storing history

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stack>
+#include<fstream>
+#include<vector>
 using namespace std;
 
 int prec(char c){
@@ -8,7 +10,7 @@ int prec(char c){
     return -1;
 }
 
-void solve_postfix(string &str){
+int solve_postfix(string &str){
     stack<int> s;
     for(int i = 0; i < str.length(); i++){
         if(str[i] == '+' or str[i] == '-' or str[i] == '*' or str[i] == '/') {
@@ -39,12 +41,11 @@ void solve_postfix(string &str){
             s.push(stoi(operand));
         }
     }
-    cout << "Result: " << s.top() << endl;
+    return s.top();
 }
 
 void infix_to_postfix(string& str, string &res){
     stack<char> s;
-    res = "";
     for(int i = 0; i < str.length(); i++){
         if(isspace(str[i])) continue;
         if(isdigit(str[i])) {
@@ -76,11 +77,70 @@ void infix_to_postfix(string& str, string &res){
 
 int main()
 {
-    string exp;
-    cout << "Enter expresion: ";
-    getline(cin, exp);
-    string postfix;
-    infix_to_postfix(exp, postfix);
-    solve_postfix(postfix);
+    int choice;
+    while(true){
+        cout << endl;
+        cout << "Enter expression for evaluation" << endl;
+        cout << "Enter 'h' To watch history" << endl;
+        cout << "Enter 'c' To clear history" << endl;
+        cout << "Enter ctrl + c To exit" << endl;
+        cout << "Enter: ";
+        string exp;
+        cout << "Enter expreation: ";
+        getline(cin, exp);
+
+        // import history data;
+        vector<pair<string,string>> history;
+        ifstream impot;
+        impot.open("history.txt");
+        while(!impot.eof()){
+            string exp, res;
+            getline(impot, exp);
+            getline(impot, res);
+            history.push_back({exp, res});
+        }
+        if(!history.empty()) history.pop_back();
+        impot.close();
+
+        // to check history
+        if(exp == "h"){
+            if(history.empty()) {
+                cout << "History is empty..!" << endl;
+                continue;
+            }
+            for(int i = 0; i < history.size(); i++){
+                cout << "Expresion: " << history[i].first << endl;
+                cout << "Result: " << history[i].second << endl;
+            }
+        }
+        // to clear history
+        else if(exp == "c"){
+            if(history.empty()) {
+                cout << "histroy is already empty..!" << endl;
+                continue;
+            }
+            history.clear();
+            ofstream expot;
+            expot.open("history.txt");
+            expot.close();
+            cout << "History is cleared...!" << endl;
+        }
+
+        // evaluate expreasion
+        else{
+            string postfix = "";
+            infix_to_postfix(exp, postfix);
+            int res = solve_postfix(postfix);
+            cout << res << endl;
+            ofstream expot;
+            expot.open("history.txt");
+            history.push_back({exp, to_string(res)});
+            for(int i = 0; i < history.size(); i++){
+                expot << history[i].first << endl;
+                expot << history[i].second << endl;
+            }
+            expot.close();
+        }
+    }
     return 0;
 }
